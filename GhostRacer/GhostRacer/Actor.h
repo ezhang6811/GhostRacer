896540d096165022952c525de6e618c7776a0410
ezhang6811 <eric.zhang6811@gmail.com>
2021-02-17 01:35:05 -0800

    changed Actor.h

--- GhostRacer/GhostRacer/Actor.h
+++ GhostRacer/GhostRacer/Actor.h
@@ -4,5 +4,11 @@
 #include "GraphObject.h"
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class Actor : public GraphObject
+{
+public:
+	Actor(int imageID, double startX, double startY, int dir = 0, double size = 1.0, unsigned int depth = 0)
+		: GraphObject(int imageID, double startX, double startY, int dir = 0, double size = 1.0, unsigned int depth = 0);
+};
 
 #endif // ACTOR_H_
