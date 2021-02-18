3332f7a4ecbe350d6c2e8efc49c17483e88d4322
ezhang6811 <eric.zhang6811@gmail.com>
2021-02-18 00:07:12 -0800

    started GhostRacer class

--- GhostRacer/GhostRacer/Actor.cpp
+++ GhostRacer/GhostRacer/Actor.cpp
@@ -2,3 +2,11 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+
+
+//ghost racer implementations
+void GhostRacer::doSomething()
+{
+
+}
--- GhostRacer/GhostRacer/Actor.h
+++ GhostRacer/GhostRacer/Actor.h
@@ -8,7 +8,13 @@ class Actor : public GraphObject
 {
 public:
 	Actor(int imageID, double startX, double startY, int dir = 0, double size = 1.0, unsigned int depth = 0)
-		: GraphObject(int imageID, double startX, double startY, int dir = 0, double size = 1.0, unsigned int depth = 0);
+		: GraphObject(imageID, startX, startY, dir, size, depth) {}
+	virtual void doSomething() = 0;
+};
+
+class GhostRacer : public Actor
+{
+	virtual void doSomething()
 };
 
 #endif // ACTOR_H_
